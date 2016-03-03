079407b2bf2b0e9c37d21918a9bd5a0510124dd2
Kevzlou7979 <kevzlou7979@gmail.com>
2016-03-03 18:46:56 +0800

    Pattern #2 : Contacts progress of the Ui Concepts.

--- src/main/java/com/github/gwtmaterialdesign/client/application/googlecontacts/GoogleContactsView.java
+++ src/main/java/com/github/gwtmaterialdesign/client/application/googlecontacts/GoogleContactsView.java
@@ -1,10 +1,16 @@
 package com.github.gwtmaterialdesign.client.application.googlecontacts;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import gwt.material.design.client.ui.MaterialIcon;
+import gwt.material.design.client.ui.MaterialNavBar;
+import gwt.material.design.client.ui.MaterialSearch;
 
 import javax.inject.Inject;
 
@@ -13,8 +19,30 @@ public class GoogleContactsView extends ViewImpl implements GoogleContactsPresen
     interface Binder extends UiBinder<Widget, GoogleContactsView> {
     }
 
+    @UiField
+    MaterialNavBar appNav, searchNav;
+
+    @UiField
+    MaterialSearch search;
+
+    @UiField
+    MaterialIcon btnSearch;
+
     @Inject
     GoogleContactsView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
+        search.addCloseHandler(new CloseHandler<String>() {
+            @Override
+            public void onClose(CloseEvent<String> event) {
+                appNav.setVisible(true);
+                searchNav.setVisible(false);
+            }
+        });
+    }
+
+    @UiHandler("btnSearch")
+    void onSearch(ClickEvent e) {
+        appNav.setVisible(false);
+        searchNav.setVisible(true);
     }
 }
